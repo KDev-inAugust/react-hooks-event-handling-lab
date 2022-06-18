2c3f0dfd8312a65642b8179deb82cb49253d40f7
Kane Mathis <admin@kanemathis.com>
2022-06-18 12:02:28 -0400

    commit tree 7267c9917bf0b45c1a86a1acf85284b3da70f988

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+
+function focusFunction(){
+    console.log('Good!')
+}
+
+function blurFunction(){
+    console.log('Hey! Eyes on me!');
+}
+
+function EyesOnMe(){
+    return(
+        <button onFocus={focusFunction} onBlur={blurFunction}>'Eyes on me'</button>
+    )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,16 @@
-// Code Keypad Component Here
+import React from "react";
+
+function changeLog(){
+    console.log('Entering password...')
+}
+
+function Keypad(){
+    return(
+       <div>
+        <input type="password" onChange={changeLog}/>
+        <h1>test</h1>
+        </div> 
+    )
+}
+
+export default Keypad
